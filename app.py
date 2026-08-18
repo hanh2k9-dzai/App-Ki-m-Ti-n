3183761cc314c0183a2c7d896db084229f5aed0f
lehanh <taovanluc45@gmail.com>
2026-08-19 05:39:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>App Kiếm Tiền</h1>
-    <p>Web đang hoạt động!</p>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
