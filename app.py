9e7726c42e31c55e412bac99b3fce3f0df88a69e
lehanh <taovanluc45@gmail.com>
2026-08-19 05:30:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return """
-    <h1>App Kiếm Tiền</h1>
-    <p>Web đang hoạt động!</p>
-    """
-
-if __name__ == "__main__":
-    app.run(debug=True)
+Trang chủ
+   ↓
+Danh sách câu hỏi
+   ↓
+Trả lời
+   ↓
+Đúng → + điểm
+Sai → không cộng
